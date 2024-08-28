12d1cd197aa1ea003d09a25841ac6816b5270d3d
mikehoe <luboskohut.shop@gmail.com>
2024-08-28 09:02:05 +0200

    hello2, hello3, add, add2

--- viewer/views.py
+++ viewer/views.py
@@ -7,6 +7,21 @@ from django.shortcuts import render
 def hello(request):
     return HttpResponse("Hello, world!")
 
+
 def hello2(request, s):
     return HttpResponse(f"Hello, {s} world!")
 
+
+def hello3(request):
+    s = request.GET.get('s', '')
+    return HttpResponse(f"Hello, {s} world!")
+
+
+def add(request, num1, num2):
+    return HttpResponse(f"{num1} + {num2} = {num1 + num2}")
+
+
+def add2(request):
+    num1 = int(request.GET.get('num1', 0))
+    num2 = int(request.GET.get('num2', 0))
+    return HttpResponse(f"{num1} + {num2} = {num1 + num2}")
