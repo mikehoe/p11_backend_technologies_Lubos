c514ad8bb8ac1f13fc0b4f7aed16411b5401a5b1
mikehoe <luboskohut.shop@gmail.com>
2024-09-11 22:38:03 +0200

    CreatorModelForm: optimalization of clean methods

--- viewer/forms.py
+++ viewer/forms.py
@@ -61,38 +61,25 @@ class CreatorModelForm(ModelForm):
     # Validace a úprava pole "name"
     def clean_name(self):
         print("Method clean_name()")
-        name = self.cleaned_data['name']
-        print(f"initial name: '{name}'")
+        name = self.cleaned_data.get('name', '')  # Použití .get() s výchozí hodnotou
         if name:
-            name = name.strip()
-            print(f"stripped name: '{name}'")
-            names = name.split()
-            if len(names):
-                names_capitalized = []
-                for name in names:
-                    names_capitalized.append(name.capitalize())
-                    print(f"names_capitalized: '{names_capitalized}'")
-                name = ' '.join(names_capitalized)
-                print(f"capitalized name: '{name}'")
+            name = ' '.join([n.capitalize() for n in name.strip().split()])
+        print(f"capitalized name: '{name}'")
         return name
 
     # Validace a úprava pole "surname"
     def clean_surname(self):
         print("Method clean_surname()")
-        surname = self.cleaned_data['surname']
-        print(f"initial surname: '{surname}'")
+        surname = self.cleaned_data.get('surname', '')  # Použití .get() s výchozí hodnotou
         if surname:
-            surname = surname.strip()
-            print(f"stripped surname: '{surname}'")
-            if len(surname):
-                surname = surname.capitalize()
-                print(f"capitalized surname: '{surname}'")
+            surname = surname.strip().capitalize()
+        print(f"capitalized surname: '{surname}'")
         return surname
 
     # Validace data narození
     def clean_date_of_birth(self):
         print("Method clean_date_of_birth()")
-        date_of_birth = self.cleaned_data['date_of_birth']
+        date_of_birth = self.cleaned_data.get('date_of_birth')
         if date_of_birth and date_of_birth >= date.today():
             raise ValidationError('Lze zadávat datum narození pouze v minulosti')
         return date_of_birth
@@ -100,7 +87,7 @@ class CreatorModelForm(ModelForm):
     # Validace data úmrtí
     def clean_date_of_death(self):
         print("Method clean_date_of_death()")
-        date_of_death = self.cleaned_data['date_of_death']
+        date_of_death = self.cleaned_data.get('date_of_death')
         if date_of_death and date_of_death >= date.today():
             raise ValidationError('Lze zadávat datum úmrtí pouze v minulosti')
         return date_of_death
@@ -111,13 +98,10 @@ class CreatorModelForm(ModelForm):
         cleaned_data = super().clean()
 
         # Ověření, že alespoň jedno z polí "name" nebo "surname" je vyplněné
-        name = cleaned_data.get('name', '')
-        surname = cleaned_data.get('surname', '')
-        if not name:
-            name = ''
-        if not surname:
-            surname = ''
-        if not len(name.strip()) and not len(surname.strip()):
+        name = cleaned_data.get('name')
+        surname = cleaned_data.get('surname')
+
+        if not name and not surname:
             raise ValidationError('Je potřeba zadat jméno nebo příjmení')
 
         # Ověření, že datum narození je menší než datum úmrtí
@@ -126,3 +110,5 @@ class CreatorModelForm(ModelForm):
 
         if date_of_birth and date_of_death and date_of_birth >= date_of_death:
             raise ValidationError('Datum narození musí být před datem úmrtí')
+
+        return cleaned_data
