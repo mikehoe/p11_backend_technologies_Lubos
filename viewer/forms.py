370e8cbb128bbc6bce1b0b1440f9cca5275c3a19
mikehoe <luboskohut.shop@gmail.com>
2024-09-12 11:38:02 +0200

    Rebase - delete unused code.

--- viewer/forms.py
+++ viewer/forms.py
@@ -1,51 +1,9 @@
 from datetime import date
 
 from django.core.exceptions import ValidationError
-from django.forms import Form, CharField, DateField, ModelChoiceField, Textarea, ModelForm, NumberInput
+from django.forms import DateField, ModelForm, NumberInput
 
-from viewer.models import Country, Creator
-
-
-class CreatorForm(Form):
-    name = CharField(max_length=32, required=False)
-    surname = CharField(max_length=32, required=False)
-    date_of_birth = DateField(required=False)
-    date_of_death = DateField(required=False)
-    country_of_birth = ModelChoiceField(queryset=Country.objects, required=False)
-    country_of_death = ModelChoiceField(queryset=Country.objects, required=False)
-    biography = CharField(widget=Textarea, required=False)
-
-    def clean_name(self):
-        cleaned_data = super().clean()
-        initial = cleaned_data['name']
-        print(f"initial name: '{initial}'")
-        result = initial
-        if initial is not None:
-            result = initial.strip()
-            print(f"result: '{result}'")
-            if len(result):
-                result = result.capitalize()
-            print(f"result: '{result}'")
-        return result
-
-    def clean_date_of_birth(self):
-        cleaned_data = super().clean()
-        date_of_birth = cleaned_data['date_of_birth']
-        if date_of_birth and date_of_birth >= date.today():
-            raise ValidationError('Lze zadávat datum narození pouze v minulosti')
-        return date_of_birth
-
-    def clean(self):
-        cleaned_data = super().clean()
-        name = cleaned_data['name']
-        surname = cleaned_data['surname']
-        if name is None:
-            name = ''
-        if surname is None:
-            surname = ''
-        if len(name.strip()) == 0 and len(surname.strip()) == 0:
-            raise ValidationError('Je potřeba zadat jméno nebo příjmení')
-        # TODO: pokud jsou zadaná data narození a úmrtí, tak datum narození musí být < datum úmrtí
+from viewer.models import Creator
 
 
 class CreatorModelForm(ModelForm):
--- viewer/views.py
+++ viewer/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy, reverse
-from django.views import View
-from django.views.generic import TemplateView, ListView, FormView, CreateView, UpdateView, DeleteView
-
 from logging import getLogger
+
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from viewer.forms import CreatorModelForm
 from viewer.models import Movie, Creator, Genre, Country
@@ -16,27 +15,7 @@ def home(request):
     return render(request, "home.html")
 
 
-def movies(request):
-    movies_ = Movie.objects.all()
-    context = {'movies': movies_}
-    return render(request, "movies.html", context)
-
-
 # Class-Based View (CBV)
-## View class
-class MoviesView(View):
-    def get(self, request):
-        movies_ = Movie.objects.all()
-        context = {'movies': movies_}
-        return render(request, "movies.html", context)
-
-
-## TemplateView class
-class MoviesTemplateView(TemplateView):
-    template_name = "movies.html"
-    extra_context = {'movies': Movie.objects.all()}
-
-
 ## ListView class
 class MoviesListView(ListView):
     template_name = "movies.html"
@@ -44,15 +23,6 @@ class MoviesListView(ListView):
     # pozor: do template se posílají data jako 'object_list'
     # můžeme to přejmenovat:
     context_object_name = 'movies'
-
-    # pokud bych potřeboval jen nějakou podmnožinu dat (filtr), lze předefinovat context data:
-    # pokud chci pouze Krimi filmy:
-    """def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        crime_genre = Genre.objects.get(name="Krimi")
-        crime_movies = Movie.objects.filter(genres=crime_genre)
-        context['movies'] = crime_movies
-        return context"""
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -69,25 +39,7 @@ def movie(request, pk):
         context = {'movie': movie_}
         # print(movie_)
         return render(request, "movie.html", context)
-    return movies(request)
-
-
-def creators(request):
-    creators_ = Creator.objects.all()
-    context = {'creators': creators_}
-    return render(request, "creators.html", context)
-
-
-class CreatorsView(View):
-    def get(self, request):
-        creators_ = Creator.objects.all()
-        context = {'creators': creators_}
-        return render(request, "creators.html", context)
-
-
-class CreatorsTemplateView(TemplateView):
-    template_name = "creators.html"
-    extra_context = {'creators': Creator.objects.all()}
+    return redirect('movies')
 
 
 class CreatorsListView(ListView):
@@ -101,38 +53,6 @@ def creator(request, pk):
         creator_ = Creator.objects.get(id=pk)
         return render(request, "creator.html", {'creator': creator_})
     return redirect('creators')
-
-
-"""
-class CreatorCreateView(FormView):
-    template_name = 'form.html'
-    form_class = CreatorModelForm
-    success_url = reverse_lazy('creators')
-
-    def form_valid(self, form):
-        result = super().form_valid(form)
-        cleaned_data = form.cleaned_data
-        name = cleaned_data['name']
-        if name is None:
-            name = ''
-        surname = cleaned_data['surname']
-        if surname is None:
-            surname = ''
-        Creator.objects.create(
-            name=name,
-            surname=surname,
-            date_of_birth=cleaned_data['date_of_birth'],
-            date_of_death=cleaned_data['date_of_death'],
-            country_of_birth=cleaned_data['country_of_birth'],
-            country_of_death=cleaned_data['country_of_death'],
-            biography=cleaned_data['biography']
-        )
-        return result
-
-    def form_invalid(self, form):
-        LOGGER.warning('User provided invalid data.')
-        return super().form_invalid(form)
-"""
 
 
 # Nemusíme psát metodu form_valid(), pokud nijak speciálně neupravujeme (nejlepší je to upravit v clean metodach())
