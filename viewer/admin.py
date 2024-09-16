0ea4763e35590c8f91ca56f6019824337f9c50da
mikehoe <luboskohut.shop@gmail.com>
2024-09-16 14:31:01 +0200

    Permissions + Admin panel.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from viewer.models import Genre, Country, Movie, Creator
+
+
+class MovieAdmin(ModelAdmin):
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        for obj in queryset:
+            obj.description = ""
+
+    ordering = ['title_orig']
+    list_display = ['id', 'title_orig', 'title_cz', 'released']
+    list_display_links = ['id', 'title_orig']
+    list_per_page = 20
+    list_filter = ['genres', 'countries']
+    search_fields = ['title_orig', 'title_cz']
+    actions = ['cleanup_description']
+
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Country)
-admin.site.register(Movie)
 admin.site.register(Creator)
-
+admin.site.register(Movie, MovieAdmin)
--- viewer/views.py
+++ viewer/views.py
@@ -1,7 +1,7 @@
 from logging import getLogger
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
@@ -58,32 +58,40 @@ def creator(request, pk):
     return redirect('creators')
 
 
+class StaffRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_staff
+
+
 # Nemusíme psát metodu form_valid(), pokud nijak speciálně neupravujeme (nejlepší je to upravit v clean metodach())
-class CreatorCreateView(LoginRequiredMixin, CreateView):
+class CreatorCreateView(PermissionRequiredMixin, CreateView):
     template_name = 'form.html'
     form_class = CreatorModelForm
     success_url = reverse_lazy('creators')
+    permission_required = 'viewer.add_creator'
 
     def form_invalid(self, form):
         LOGGER.warning('User provided invalid data.')
         return super().form_invalid(form)
 
 
-class CreatorUpdateView(LoginRequiredMixin, UpdateView):
+class CreatorUpdateView(PermissionRequiredMixin, UpdateView):
     template_name = 'form.html'
     form_class = CreatorModelForm
     success_url = reverse_lazy('creators')
     model = Creator
+    permission_required = 'viewer.change_creator'
 
     def form_invalid(self, form):
         LOGGER.warning('User provided invalid data.')
         return super().form_invalid(form)
 
 
-class CreatorDeleteView(LoginRequiredMixin, DeleteView):
+class CreatorDeleteView(PermissionRequiredMixin, DeleteView):
     template_name = 'confirm_delete.html'
     model = Creator
     success_url = reverse_lazy('creators')
+    permission_required = 'viewer.delete_creator'
 
 
 class GenreView(View):
